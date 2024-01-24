fc5d7466122d600419fd6f822188feb49a851f74
araluce <alvaro.fernandez@crisalix.com>
2024-01-24 13:05:30 +0100

    Blueprint

--- lib/repo_sync.rb
+++ lib/repo_sync.rb
@@ -4,5 +4,13 @@ require_relative "repo_sync/version"
 
 module RepoSync
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.sync_repos
+    #Dir.chdir "#{Dir.home}/projects" do
+      #REPO_LIST.each do |dir_name|
+        #RepoDir.new(dir_name).sync
+        #puts "\n\n\n"
+      #end
+    #end
+  end
 end
